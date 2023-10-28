6cf075f75e2a92fddfb29324ae8fe4c3bf94954a
Alexander Ivanov <sashapop10@yandex.ru>
2023-10-28 20:42:59 +0300

    1.2.0

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const [fs, { join, sep }] = [require('node:fs'), require('node:path')];
 const { access: accessible, scheduler } = require('./lib');
 const { EventEmitter } = require('node:events');
 
-module.exports = function Watcher(options) {
+module.exports = function Watcher(options = {}) {
+  const { timeout, ignore = [], home, deep } = options;
   const [watchers, bridge] = [new Map(), new EventEmitter()];
-  const emit = scheduler(options?.timeout, bridge.emit.bind(bridge));
-  const access = accessible.bind(null, options?.ignore ?? []);
+  const emit = scheduler(timeout, bridge.emit.bind(bridge));
+  const access = accessible.bind(null, [...ignore]);
 
   const lookup = path => (err, files) =>
     void (!err && files.forEach(f => f.isDirectory() && bridge.watch(join(path, f.name))));
@@ -17,9 +18,9 @@ module.exports = function Watcher(options) {
     const target = path.endsWith(sep + filename) ? path : join(path, filename);
     if (!access(target)) return;
     fs.stat(target, (err, stats) => {
-      const parsed = options?.home ? target.replace(options.home, '') : target;
+      const parsed = home ? target.replace(home, '') : target;
       if (err) return void (bridge.unwatch(target), emit('delete', parsed));
-      stats.isDirectory() && options?.deep && fs.readdir(target, READ_OPTS, lookup(path));
+      stats.isDirectory() && deep && fs.readdir(target, READ_OPTS, lookup(path));
       return void emit('change', parsed);
     });
   };
@@ -32,7 +33,7 @@ module.exports = function Watcher(options) {
     fs.stat(path, (err, stats) => {
       if (err || watchers.has(path)) return;
       watchers.set(path, fs.watch(path, listener(path)));
-      stats.isDirectory() && options?.deep && fs.readdir(path, READ_OPTS, lookup(path));
+      stats.isDirectory() && deep && fs.readdir(path, READ_OPTS, lookup(path));
     });
     return bridge;
   };
